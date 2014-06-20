934e310159c4867f719ce59b8b780dbdf1b5eeff
Phil Roberts <philr91@hotmail.com>
2014-06-20 15:40:53 +0100

    Update config.php

--- phpinc/config.php
+++ phpinc/config.php
@@ -1,6 +1,6 @@
 <?php
     //Database Connection Constants
     define("DB_HOST", "localhost");
-    define("DB_USER", "root");
-    define("DB_PASS", "mysql");
+    define("DB_USER", "username");
+    define("DB_PASS", "password");
     define("DB_NAME", "simplelogin");
